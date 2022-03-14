6edbf6715e11bd94e7332b669c34f90154cb10ea
Samppa Alatalo <samppa.alatalo@gmail.com>
2022-03-14 16:33:43 +0200

    implement-shufflebag

--- RNG.cpp
+++ RNG.cpp
@@ -7,3 +7,40 @@ int rng(int rangeFrom, int rangeTo) {
     uniform_int_distribution<int> distr(rangeFrom, rangeTo);
     return distr(generator);
 }
+
+template <typename T>
+void shuffleVector(vector<T> * contents) {
+    shuffle(contents->begin(), contents->end(), generator);
+}
+
+template <typename T>
+ShuffleBag<T>::ShuffleBag(int initCapacity)
+{
+    data.resize(initCapacity);
+    currentPosition = -1;
+    capacity = data.capacity();
+    size = data.size();
+}
+
+template <typename T>
+void ShuffleBag<T>::add(T item, int amount) {
+    for (int i = 0; i < amount; i++) {
+        data.push_back(item);
+    }
+    currentPosition = size - 1;
+}
+
+template <typename T>
+T ShuffleBag<T>::next() {
+    if (currentPosition < 1) {
+        currentPosition = size - 1;
+        currentItem = data[0];
+        return currentItem;
+    }
+    int pos = rng(0, currentPosition);
+    currentItem = data[pos];
+    data[pos] = data[currentPosition];
+    data[currentPosition] = currentItem;
+    currentPosition--;
+    return currentItem;
+}
--- RNG.h
+++ RNG.h
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <algorithm>
 #include <random>
+#include <vector>
 
 #ifndef RNG_H
 #define RNG_H
@@ -7,5 +9,24 @@
 using namespace std;
 
 int rng(int, int);
+template <typename T>
+void shuffleVector(vector<T> *);
+
+template <typename T>
+class ShuffleBag
+{
+private:
+    vector<T> data;
+    T currentItem;
+    int currentPosition;
+    int capacity;
+public:
+    int size;
+    ShuffleBag(int);
+    void add(T, int);
+    T next();
+};
+
+#include "RNG.cpp"
 
 #endif

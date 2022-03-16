d000b7f15a60781cd2d1519165613f1dee92a7e8
Samppa Alatalo <samppa.alatalo@gmail.com>
2022-03-16 22:37:46 +0200

    Set up tasks.json and bring the code to a compilable state from prototyping

--- Dungeon.h
+++ Dungeon.h
@@ -14,6 +14,7 @@ public:
     Player player;
     Room rooms[4];
     Dungeon(Player);
+    void generateDungeon(int, int); // MATRIISILUONTI?
     int runDungeon();
     void enterRoom(Room *);
     char handleInput(int, string[], vector<char>, bool=true, bool=true);
